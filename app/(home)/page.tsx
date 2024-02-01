42f66a2a278ad22f10acf582ca052c3c8e0d3d0d
tami <tamiortami@gmail.com>
2024-02-01 16:18:07 +0900

    feat: main page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,80 @@
-"use client";
-
 import Image from "next/image";
-import useSWR from "swr";
+import Link from "next/link";
 
-export default function Home() {
+export const metaData = {
+  title: "Home",
+};
+
+export const API_URL = "https://rickandmortyapi.com/api";
+
+async function getCharacters() {
+  const response = await fetch(`${API_URL}/character`);
+  const json = await response.json();
+  return json;
+}
+
+export default async function Home() {
   // swr --------------------------------------------------------------------------------------- swr
-  const { data } = useSWR("api/characters", getCharacters);
-  // const characters = await getCharacters();
-  console.log("characters", data);
+  // const { data } = useSWR("api/rick-and-morty", getCharacters);
+  const characters = await getCharacters();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        {/* {data?.map((character: any) => (
-          <li key={character.id}>{character.name}</li>
-        ))} */}
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-6">
+        {characters?.results.map((character: any) => (
+          <>
+            <div
+              className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+              key={character.id}
+            >
+              <Link href={`characters/${character.id}`}>
+                <Image
+                  src={character.image}
+                  width={300}
+                  height={300}
+                  alt="character_img"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </Link>
+              <div className="p-5">
+                <Link href={`characters/${character.id}`}>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {character.name}
+                  </h5>
+                </Link>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  status: {character.status}
+                </p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  species: {character.species}
+                </p>
+
+                <Link
+                  href={`characters/${character.id}`}
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800"
+                >
+                  Read more
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </>
+        ))}
       </div>
     </main>
   );
-}
-function getCharacters(arg: "api/characters") {
-  throw new Error("Function not implemented.");
 }
