da87c7a96e6e30d82d7ee688f79026c2aa31e9fc
tami <tamiortami@gmail.com>
2024-02-01 16:17:57 +0900

    feat: api

---
+++ app/api/rick-and-morty-api.tsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+// rick and morty api
+const API_URL = "https://rickandmortyapi.com/api";
+
+// async function getCharacters() {
+//   const response = await fetch(API_URL);
+//   const json = await response.json();
+//   return json;
+// }
+
+export const getCharacters = () => {
+  return axios.get(`${API_URL}/character`).then((res) => res.data);
+};
