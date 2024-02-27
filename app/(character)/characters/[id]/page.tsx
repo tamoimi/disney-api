2bc5574f7bc91edfb656f312e908ea66097a22ba
tami <tamiortami@gmail.com>
2024-02-27 13:44:10 +0900

    feat: added Loading component

--- app/(character)/characters/[id]/page.tsx
+++ app/(character)/characters/[id]/page.tsx
@@ -1,10 +1,11 @@
+import Loading from "@/app/(home)/loading";
 import CharacterInfo from "@/app/components/detail";
 import { Suspense } from "react";
 
 export default async function CharacterDetail({ params: { id } }: { params: { id: string } }) {
   return (
     <div>
-      <Suspense fallback={<h1>Loading character info</h1>}>
+      <Suspense fallback={<Loading />}>
         <CharacterInfo id={id} />
       </Suspense>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Roboto_Mono } from "next/font/google";
 import Navigation from "./components/navigation";
 import "./globals.css";
 
-const roboto  = Roboto_Mono({ subsets: ["latin"] });
+const roboto = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Rick and Morty",
