551de290fdc930bb79d5920298c8f69243206fd4
tami <tamiortami@gmail.com>
2024-02-27 13:45:22 +0900

    style: detail style

--- app/components/detail.tsx
+++ app/components/detail.tsx
@@ -10,19 +10,20 @@ export default async function CharacterInfo({ id }: { id: string }) {
   const character = await getDetail(id);
   return (
     <>
-      <main className="flex flex-col items-center justify-center px-5">
+      <main className="flex flex-col items-center justify-center p-5">
         <Image
           src={character.image}
           width={300}
           height={300}
           alt="character_img"
-          style={{ width: "100%", height: "auto", borderRadius: "50%", marginBottom: 20 }}
+          style={{ borderRadius: "50%" }}
         />
-        <h1>Name:{character.name}</h1>
+        <div className="bg-gray-200 my-10 p-3 rounded">
         <h1>Name:{character.name}</h1>
         <h4>Status:{character.status}</h4>
         <h4>Origin:{character.origin.name}</h4>
         <h4>Gender:{character.gender}</h4>
+        </div>
       </main>
     </>
   );
