9a37dec878f3089d79077f6c8434e47cba8fa089
tami <tamiortami@gmail.com>
2024-03-21 16:04:41 +0900

    style: tailwind floating label

--- app/components/detail.tsx
+++ app/components/detail.tsx
@@ -10,7 +10,7 @@ export default async function CharacterInfo({ id }: { id: string }) {
   const character = await getDetail(id);
   return (
     <>
-      <main className="flex flex-col items-center justify-center p-5">
+      <main className="flex flex-col items-center justify-center p-6">
         <Image
           src={character.image}
           width={300}
@@ -18,12 +18,57 @@ export default async function CharacterInfo({ id }: { id: string }) {
           alt="character_img"
           style={{ borderRadius: "50%" }}
         />
-        <div className="bg-gray-200 my-10 p-3 rounded">
-          <h1>Name:{character.name}</h1>
-          <h4>Status:{character.species}</h4>
-          <h4>Species:{character.status}</h4>
-          <h4>Origin:{character.origin.name}</h4>
-          <h4>Gender:{character.gender}</h4>
+        <div className="flex flex-col gap-3 w-full mt-5">
+          <div className="relative">
+            <input
+              type="text"
+              className="block px-2.5 pb-2.5 pt-4 w-full text-gray-900  rounded-lg appearance-none  focus:outline-none focus:teal-blue-600 peer bg-transparent border border-gray-300"
+              value={character.name}
+            />
+            <label className="absolute bg-gray-100 text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-teal-600  peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+              Name
+            </label>
+          </div>
+          <div className="relative">
+            <input
+              type="text"
+              className="block px-2.5 pb-2.5 pt-4 w-full text-gray-900  rounded-lg appearance-none  focus:outline-none focus:teal-blue-600 peer bg-transparent border border-gray-300"
+              value={character.status}
+            />
+            <label className="absolute bg-gray-100 text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-teal-600  peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+              Status
+            </label>
+          </div>
+          <div className="relative">
+            <input
+              type="text"
+              className="block px-2.5 pb-2.5 pt-4 w-full text-gray-900  rounded-lg appearance-none  focus:outline-none focus:teal-blue-600 peer bg-transparent border border-gray-300"
+              value={character.species}
+            />
+            <label className="absolute bg-gray-100 text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-teal-600  peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+              Species
+            </label>
+          </div>
+          <div className="relative">
+            <input
+              type="text"
+              className="block px-2.5 pb-2.5 pt-4 w-full text-gray-900  rounded-lg appearance-none  focus:outline-none focus:teal-blue-600 peer bg-transparent border border-gray-300"
+              value={character.origin.name}
+            />
+            <label className="absolute bg-gray-100 text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-teal-600  peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+              Origin
+            </label>
+          </div>
+          <div className="relative">
+            <input
+              type="text"
+              className="block px-2.5 pb-2.5 pt-4 w-full text-gray-900  rounded-lg appearance-none  focus:outline-none focus:teal-blue-600 peer bg-transparent border border-gray-300"
+              value={character.gender}
+            />
+            <label className="absolute bg-gray-100 text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-teal-600  peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+              Gender
+            </label>
+          </div>
         </div>
       </main>
     </>
