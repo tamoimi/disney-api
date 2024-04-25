d2c5c05575999e65948eef7b33e352773bcf7ee0
tami <tamiortami@gmail.com>
2024-04-25 17:06:24 +0900

    error: next.config img error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   domains: ["rickandmortyapi.com"],
+  // },
   images: {
-    domains: ["rickandmortyapi.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rickandmortyapi.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
